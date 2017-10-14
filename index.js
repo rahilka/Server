5a0471832236c579084d3ec3aab86d2cf1471a5e
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-10-14 17:59:14 +0200

    Inital commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+// meaning: in development environment we'll use port 5000
+// and in production environment we'll use whatever port Heroku is attempting to provide to us
+app.listen(PORT);
