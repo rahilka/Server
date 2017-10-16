6aecc5d618c36a30d1c71c8f6599a88a24a342d7
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-10-16 17:22:55 +0200

    Refactor index.js

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+const express = require('express');
+// const passportConfig = require('./services/passport'); Not necessary
+require('./services/passport'); // because we do not return anything from that file
 
 const app = express();
 
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: "/auth/google/callback"
-		},
-		(accessToken, refreshToken, profile, done) => {
-			console.log("accessToken: ", accessToken);
-			console.log("refreshToken: ", refreshToken);
-			console.log("profile ", profile);
-		}
-	)
-);
-
-app.get(
-	"/auth/google",
-	passport.authenticate("google", {
-		scope: ["profile", "email"]
-	})
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+require('./routes/authRoutes')(app); //We immediately invoke the function return from the authRoutes file
+// !!! the authRoutes file returns a function
+// the code above is the same as doing the next:
+// const authRoutes = require('./routes/authRoutes');
+// authRoutes(app);
+// It's just that is not mandatory to create a variable for it !!!
 
 const PORT = process.env.PORT || 5000;
 // meaning: in development environment we'll use port 5000
