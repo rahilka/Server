0bed41ebce1b6631485ea383164803541713b28f
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-10-16 14:19:47 +0200

    Testing oauth

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
-const keys = require('./config/keys');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express();
 
 passport.use(
-	new GoogleStrategy({
-		clientID: keys.googleClientID,
-		clientSecret: keys.googleClientSecret,
-		callbackURL: '/auth/google/callback'
-	}, (accessToken) => {
-		console.log(accessToken);
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: "/auth/google/callback"
+		},
+		accessToken => {
+			console.log(accessToken);
+		}
+	)
+);
+
+app.get(
+	"/auth/google",
+	passport.authenticate("google", {
+		scope: ["profile", "email"]
 	})
 );
-// 'new GoogleStrategy(...)' creates a new instance of the Google Strategy
-// 'passport.use()' meaning: use the passed in strategy
 
 const PORT = process.env.PORT || 5000;
 // meaning: in development environment we'll use port 5000
