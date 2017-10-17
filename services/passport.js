079f173ab8ffd08814ee17f8b84c7e3610b0a035
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-10-17 14:57:30 +0200

    Added proxy true property to google strategy options

--- services/passport.js
+++ services/passport.js
@@ -28,7 +28,8 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
+			callbackURL: '/auth/google/callback',
+			proxy: true //meaning: if our request runs throug any proxy, that's tottaly fine, trust the proxy and calculate the callback url properly
 		},
 		(accessToken, refreshToken, profile, done) => {
 			User.findOne({ googleId: profile.id }).then(existingUser => {
