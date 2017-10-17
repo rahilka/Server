ca7788b0e451298370d36aa90f0dedffbe7486b7
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-10-17 12:33:39 +0200

    Logout user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,11 @@ module.exports = app => {
 
 	app.get('/auth/google/callback', passport.authenticate('google'));
 
+	app.get('/api/logout', (req, res) => {
+		req.logout(); // this function is automatically attached to the req object by 'passport'
+		res.send(req.user); // prove that the user is not signed in anymore
+	});
+
 	app.get('/api/current_user', (req, res) => {
 		// immediately send the response = user
 		res.send(req.user);
