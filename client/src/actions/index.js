b7c1f386cdf1434e924bd3a595f9e394785d746c
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-10-18 21:30:40 +0200

    FInish auth reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,7 +4,7 @@ import { FETCH_USER } from './types';
 export const fetchUser = () => async dispatch => {
 	const res = await axios.get('/api/current_user');
 	
-	dispatch({ type: FETCH_USER, payload: res });
+	dispatch({ type: FETCH_USER, payload: res.data });
 
 	// the above two lines can be refactored to one line of code: 
 	// dispatch({ type: FETCH_USER, payload: await axios.get('/api/current_user') });
