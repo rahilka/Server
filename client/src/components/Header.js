c14da4c62228f59abbc5534c14ce38a2039c2099
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-10-18 21:45:34 +0200

    Added header content

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,11 +5,13 @@ class Header extends Component {
 	renderContent() {
 		switch (this.props.auth) {
 			case null:
-				return 'Still deciding';
+				return;
 			case false:
-				return 'Logged out';
+				return (
+					<li><a href="/auth/google">Login With Google</a></li>
+				);
 			default:
-				return 'Logged in';
+				return <li><a>Logout</a></li>;
 		}
 	}
 
