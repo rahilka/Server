d946df2cabce0c7ade6626852c37e8f504b5df88
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-10-19 15:06:26 +0200

    Added body parser module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User'); // on app startup create the model(schema), if does not exist
 // const passportConfig = require('./services/passport'); Not necessary
@@ -11,13 +12,13 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000, //30 days in milliseconds
 		keys: [keys.cookieKey]
 	})
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
