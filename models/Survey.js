042c1acbc223cf2c2dbb68b9987358047fcfb23e
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-10-20 16:32:17 +0200

    Added relationship field of the user model, added date fields

--- models/Survey.js
+++ models/Survey.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
 	title: String,
 	body: String,
 	subject: String,
-	recipients: [String],
+	recipients: [RecipientSchema],
 	yes: { type: Number, default: 0 }, 
-	no: { type: Number, default: 0 }
+	no: { type: Number, default: 0 },
+	_user: { type: Schema.Types.ObjectId, ref: 'User' }, //the reference that we are making here belongs to the 'User's collection
+	// the '_' means that it is a relation between this model and another one
+	dateSent: Date,
+	lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
