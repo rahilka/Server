ce462a69201dcc337c39419b91871dd35baa768b
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-10-21 14:06:27 +0200

    Finished mailer methods/properties

--- services/Mailer.js
+++ services/Mailer.js
@@ -29,6 +29,15 @@ class Mailer extends helper.Mail {
 		trackingSettings.setClickTracking(clickTracking);
 		this.addTrackingSettings(trackingSettings);
 	}
+
+	addRecipients() {
+		const personalize = new helper.Personalization();
+		
+		this.recipients.forEach(recipient => {
+			personalize.addTo(recipient);
+		});
+		this.addPersonalization(personalize);
+	}
 }
 
 module.exports = Mailer;
