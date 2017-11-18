fa8efb9416945317d284e0c84d9f821088b89c6a
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-11-18 16:00:44 +0100

    Wiring up custom fields

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,10 @@
 // SurveyField contains logic to render a single label and text input
 import React from 'react';
 
-export default () => {
+export default ({ input }) => {
 	retrun (
 		<div>
-			<input />
+			<input {...input} />
 		</div>
 	);
 }
