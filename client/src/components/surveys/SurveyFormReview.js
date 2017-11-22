918a40ac53a54b8c977c69764966efd79b102c62
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-11-22 21:01:15 +0100

    Added sumbit form action

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,8 +3,9 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
 	const reviewFields = _.map(formFields, ({ name, label }) => {
 		return (
 			<div key={name}>
@@ -26,6 +27,13 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
 			>
 				Back
 			</button>
+			<button 
+					onClick={() => submitSurvey(formValues)}
+					className="green white-text btn-flat right"
+				>
+				Send Survey
+				<i className="material-icons right">email</i>
+			</button>
 		</div>
 	);
 };
@@ -34,4 +42,4 @@ function mapStateToProps(state) {
 	return { formValues: state.form.formValues.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
