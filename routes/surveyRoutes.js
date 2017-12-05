3d55fb63d915b2303a7488a674699dcba6bb728e
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-12-05 20:56:52 +0100

    Fetching a list of surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,8 +10,10 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-	app.get('/api/surveys/:surveyId/:choice', (req, res) => {
-		res.send('Thanks for voting!');
+	app.get('/api/surveys', requireLogin, async (req, res) => {
+		const surveys = await Survey.find({ _user: req.user.id });
+		
+		res.send(surveys);
 	});
 
 	app.post('/api/surveys/webhooks', (req, res) => {
