f4b37a99eb36913e5433ea6e601519614a83609e
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-12-05 21:37:26 +0100

    Wire up surveys to redux

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 
 export default combineReducers({
 	auth: authReducer, //meaning: the 'auth' piece of state is produces by the authReducer
-	form: reduxForm
+	form: reduxForm,
+	survey: surveysReducer
 });
